ed7f95d74976873a1de1fe441972e01b99bf4a8c
elizielx <yehezkieldio35@gmail.com>
2023-11-16 06:43:20 +0800

    feat: some changes 2

---
+++ perulangan/src/main/java/com/elizielx/Main.java
@@ -0,0 +1,20 @@
+package com.elizielx;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int angka;
+
+        System.out.print("Masukkan berapa angka: ");
+        angka = scanner.nextInt();
+
+        for (int i = 0; i <= 0; i = i - 2) {
+            System.out.println(i);
+        }
+
+        scanner.close();
+    }
+
+}
